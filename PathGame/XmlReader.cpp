cbb0804a999abec2ac94c590b62b1a6f64368b50
DenysRisukhin <risukhindenysit@gmail.com>
2018-04-17 16:12:21 +0400

    Random and Sound fixed

--- PathGame/XmlReader.cpp
+++ PathGame/XmlReader.cpp
@@ -1,6 +1,45 @@
 #include "XmlReader.h"
-#include "InitializationException.h"
+#include "InitException.h"
 
 using namespace irr;
 using namespace core;
 using namespace io;
+
+XmlReader::XmlReader(const char* filename)
+		 : _reader(createIrrXMLReaderUTF16(filename))
+
+{
+	if (!_reader)
+		throw InitException("_reader");
+}
+
+XmlReader::~XmlReader(void)
+{
+	delete _reader;
+}
+
+bool XmlReader::next()
+{
+	if (!_reader)
+		return false;
+
+	bool result;
+	while ((result = _reader->read()) && _reader->getNodeType() == EXN_ELEMENT_END);
+
+	if (!result) {
+		delete _reader;
+		_reader = 0;
+	}
+
+	return result;
+}
+
+stringw XmlReader::getNodeName()
+{
+	return stringw(_reader->getNodeName());
+}
+
+stringw XmlReader::getAttributeValue(const stringw& name)
+{
+	return stringw(_reader->getAttributeValue((const char16*)name.c_str()));
+}
--- PathGame/XmlReader.h
+++ PathGame/XmlReader.h
@@ -3,12 +3,30 @@
 #include "IConfigReader.h"
 #include <irrlicht.h>
 
-
+/**
+* Represents xml reader that does not use Irrlicht's filesystem.
+*/
 class XmlReader : public IConfigReader
 {
 public:
+	XmlReader(const char* filename);
+	~XmlReader(void);
+
+	/**
+	* Implementation of the IConfigReader.
+	*/
 	
+	virtual bool next();
+
+	virtual irr::core::stringw getNodeName();
+
+	virtual irr::core::stringw getAttributeValue(
+		const irr::core::stringw& name);
+	virtual int getAttributeValueAsInt(
+		const irr::core::stringw& name);
+	virtual float getAttributeValueAsFloat(
+		const irr::core::stringw& name);
 
 private:
-
+	irr::io::IrrXMLReaderUTF16* _reader;
 };
